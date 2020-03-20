47d9d31ad4dbed36bae41d2dc4077c8a4ce0e9db
liziyi0914 <liziyi0914@gmail.com>
2020-03-20 23:29:46 +0800

    fix a bug

--- .umirc.js
+++ .umirc.js
@@ -1,30 +1,31 @@
 
 // ref: https://umijs.org/config/
 export default {
-  treeShaking: true,
-  routes: [
-    {
-      path: '/',
-      component: '../layouts/index',
-      routes: [
-        { path: '/', component: '../pages/index' }
-      ]
-    }
-  ],
-  plugins: [
-    // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: true,
-      dva: false,
-      dynamicImport: false,
-      title: 'MCBBS_signature',
-      dll: false,
-      
-      routes: {
-        exclude: [
-          /components\//,
-        ],
-      },
-    }],
-  ],
+	treeShaking: true,
+	publicPath: '/MCBBS-Signature',
+	routes: [
+		{
+			path: '/',
+			component: '../layouts/index',
+			routes: [
+				{ path: '/', component: '../pages/index' }
+			]
+		}
+	],
+	plugins: [
+		// ref: https://umijs.org/plugin/umi-plugin-react.html
+		['umi-plugin-react', {
+			antd: true,
+			dva: false,
+			dynamicImport: false,
+			title: 'MCBBS_signature',
+			dll: false,
+
+			routes: {
+				exclude: [
+					/components\//,
+				],
+			},
+		}],
+	],
 }
